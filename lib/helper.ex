42a39a61d9a38c2e666305395d90294d106c003d
Mickey Chen <poverwhelming@gmail.com>
2021-12-05 13:35:00 +0800

    finished day 4

--- lib/helper.ex
+++ lib/helper.ex
@@ -1,10 +1,10 @@
 defmodule Helper do
   def parse_file(filename, opts \\ []) do
     map_fn = Keyword.get(opts, :map_by, & &1)
+    split_by = Keyword.get(opts, :split_by, "\n")
 
     File.read!(filename)
-    |> String.trim()
-    |> String.split("\n")
+    |> String.split(split_by, trim: true)
     |> Enum.map(map_fn)
   end
 end
